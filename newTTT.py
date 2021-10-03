ba7f0bdc9929d1d1771e1469434a1228ae7d66d0
leejianzhi <lijianzhimomo@gmail.com>
2021-10-02 19:23:00 -0700

    Need work on basic user interaction

--- newTTT.py
+++ newTTT.py
@@ -1,13 +1,35 @@
-def user_choice():
+def check_input():
+
     choice = " "
 
-    while choice.isdigit() == False:
-        print("\n")
-        print("Please input a digit number (Ex. 1,2,3,4,5) \n")
-        choice = input("Pleae make a choice, 1 for Player 1(X), and 2 for Player 2(O) to start the game: ")
+
+    while choice not in ['0','1','2','3','4','5','6','7','8','9']:
+        choice = input("Please input a digital number through 1 - 10 to please on the position: ")
+        print_board(choice)
+
+        if choice not in ['0','1','2','3','4','5','6','7','8','9']:
+            print("Invalid input, please enter again.")
+
+
+def print_board(choice):
+
+    line_1 = ['0','1','2']
+    line_2 = ['3','4','5']
+    line_3 = ['6','7','8']
+
+    print(line_1)
+    print(line_2)
+    print(line_3)
+
+    if(choice == 0):
+        choice = line_1[0]
+
+    print(line_1)
+    print(line_2)
+    print(line_3)
 
 
 
 
 if __name__ == "__main__":
-    user_choice()
+    check_input()
