917aa2c6f0c657a996470e77de7ded05586ab6bf
leejianzhi <lijianzhimomo@gmail.com>
2021-10-25 13:37:33 -0400

    Finished chapter 2 practice

--- python_crashCourse_handsOn.py
+++ python_crashCourse_handsOn.py
@@ -9,10 +9,22 @@ def remove_space():
 	edit_message = og_message.rstrip()
 
 
-	print("OG message: ",og_message)
+	print("OG   message: ",og_message)
 	print("\n")
 	print("After remove space: ",edit_message)
 
+
+def chapter_2():
+
+	user_name = input("Please enter your full name: ")
+
+	print("Hello " + user_name.title() + ",would you like to learn some Python today?")
+
+	print(user_name.title() + " said, if you do not make all the effort for living today, you will fell sorry tomorrow")
+
+
+
 if __name__ == "__main__":
 
-	remove_space()
+	
+	chapter_2()
